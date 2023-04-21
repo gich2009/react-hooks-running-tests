491c3655148bd0909ccd872f28dcb6abd88bf3fa
gich2009 <waneneamos@gmail.com>
2023-04-21 23:28:38 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Article from './Article'
-import Header from './Header'
+import Article from './Article.js'
+import Header from './Header.js'
 
 function App() {
   return <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import Header from "./components/Header";
+import Article from "./components/Article";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
